653ff5f03fdfe0de0f1123c106e1ddffaf7e2d63
Nathanael Jones <nathanael.jones@gmail.com>
2011-09-08 20:40:31 +0200

    Moved script files to the code directory

--- code/world/house.lua
+++ code/world/house.lua
@@ -3,15 +3,19 @@ function kitchen()
 	p[[
 		You stand at the door, and survey the mess.]]
 	
-	local unwashed = this.disheswashed ~= null or this.disheswashed < os.time() - time.one_day 
+	--define_var("this.disheswashed",0)
+	--define_var("user.world.house.kitchen",0)
+	
+	local unwashed = (this.disheswashed == null) or (this.disheswashed < (os.time() - time.one_day))
 	
 	local choices = {"hallway"}
 	
 	if unwashed then 
 		p[[	Unwashed dishes rise in a precarious tower from the sink, and a trail of ants marks the location of a honey jar]]
 		choices["Wash dishes"]= function()
+		  this.disheswashed = os.time()
 			message("You scrub dishes for hours, and finally finish the last utensil. Your fingers are raw from scubbing fossilized food remains.")
-			this.disheswashed = os.time
+			
 			goto("kitchen")
 		end
 	end
@@ -30,6 +34,7 @@ end
 
 outside_ = "Go to your house"
 function outside()
+	clear()
 	p[[
 		You stand outside the house, contemplating how much it will cost to fix the roof.
 	]]
