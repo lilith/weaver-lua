e4a9e4b0a7a58f75148bef819a9eef18b3356cec
evilfrenchguy <evilfrenchguy@gmail.com>
2011-09-10 17:19:31 +0000

    Added a test folder

---
+++ code/world/test/rabbithole.lua
@@ -0,0 +1,10 @@
+rabbithole_ = "Take a leap of faith..."
+function rabbithole()
+	p[[
+		This new, fantastic world of colors and creativity stands before you. ]]
+	
+	-- Let them go back to the town
+	local choices = {"world.town.center"}
+	
+	choose(choices)
+end
